4f4a7197a593df518c112113cad16fde8ea421b7
HighTech <gaoke.yang@yahoo.com>
2020-08-10 23:10:53 -0400

    protected

--- Src/Inheritance.cpp
+++ Src/Inheritance.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 using namespace std;
 
+class Base
+{
+    int iPrivate=1;
+public:
+    int iPublic=2;
+protected:
+    int iProtected=3;
+};
 
+class Derived:public Base
+{
+public:
+    void display() 
+    {
+        cout << "From Base:" << iProtected << endl;
+    }
+};
 
 int main()
 {
+    Derived d;
+    d.display();
 
 }
